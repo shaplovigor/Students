bd3bcd99f009b77da1d5618a3beb5fe20df70684
shaplovigor <69524502+shaplovigor@users.noreply.github.com>
2020-09-16 01:01:36 +1000

    add superClass, null in sout

--- src/Module12/Students.java
+++ src/Module12/Students.java
@@ -1,65 +1,48 @@
 package Module12;
 
-public class Students {
+public class Students extends Person {
 
-    public String Name;
-    public String family;
-    public int old;
-    public int experience;
-    public String learning_objective;
-    public int group_number;
-    public int number_of_completed_tasks;
-    public int number_of_completed_modules;
+    public String name;
+    public String surname;
+    public int age;
+    public boolean experienced;
+    public String learningObjective;
+    public int group;
     private static int taskCount;
-    private static int moduleCount;
+    private static int passedModuleCount;
     private static final int MAX_MODULE = 3;
 
-    public Students(String Name, String family, int old) {
-        this(Name, family, old,0,"Learning Java",0,
-                0,0);
+    public Students(String name, String surname, int age) {
+        this(name, surname, age,false,"Learning Java",0);
     }
 
-    public Students(String Name, String family, int old, int experience,
-                   String learning_objective, int group_number,
-                   int number_of_completed_tasks, int number_of_completed_modules) {
-        this.Name = Name;
-        this.family = family;
-        this.old = old;
-        this.experience = experience;
-        this.learning_objective = learning_objective;
-        this.group_number = group_number;
-        this.number_of_completed_tasks = number_of_completed_tasks;
-        this.number_of_completed_modules = number_of_completed_modules;
+    public Students(String name, String surname, int age, boolean experienced,
+                   String learningObjective, int group) {
+        super(name, surname, age);
+        this.experienced = experienced;
+        this.learningObjective = learningObjective;
+        this.group = group;
     }
 
-    public static void task_execution(String student, int task) {
+    public static void taskExecution(String student, int task) {
         taskCount++;
         System.out.println(student + " выполнил задачу № " + task + ". Всего выполнил задач: " + taskCount);
     }
 
-    public static void module_execution(String student, int module) {
-        if( moduleCount < MAX_MODULE) {
-            moduleCount++;
-            System.out.println(student + " выполнил модуль № " + module + ". Всего выполнил модулей: " + moduleCount);
+    public static void moduleExecution(String student, int module) {
+        if( passedModuleCount < MAX_MODULE) {
+            passedModuleCount++;
+            System.out.println(student + " выполнил модуль № " + module + ". Всего выполнил модулей: " + passedModuleCount);
         }
-        else {
-            System.out.println("Для " + student + " все модули уже пройдены.");
-        }
+        else { System.out.println("Для " + student + " все модули уже пройдены."); }
     }
 
-    public void carry_out_task() {
+    public void carryOutTask() {  }
 
-    }
+    public void askAquestion() {  }
 
-    public void ask_a_question() {
+    public void passTheModule() {  }
 
-    }
+    public void communicateEithTheCurator() {  }
 
-    public void pass_the_modele() {
-
-    }
-
-    public void communicate_eith_the_curator() {
-
-    }
 }
